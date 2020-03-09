3505d7f8272c5b22fcd00d53f48161f99de954a4
Bradley J. Tannor <bradleytannor@gmail.com>
2020-03-09 12:49:48 -0700

    Typed Files
    All tests pass. The copy constructor declaration was untested.

--- lib/node_int.rb
+++ lib/node_int.rb
@@ -6,4 +6,121 @@ require "node_int/version"
 # @bastract
 # A Node abstract base class.
 class NodeInt
+
+  STRUCTURE = {back: nil, data: nil, front: nil}.freeze()
+
+  # initialize(back = nil, data = nil, front = nil).
+  # @abstract
+  # Constructor. Instantiates a node.
+  def initialize(back = nil, data = nil, front = nil)
+  end
+
+  # copy_constructor().
+  # @abstract
+  # Copy constructor.
+  def copy_constructor()
+  end
+
+  # substitute(rhs).
+  # @abstract
+  # Substitutes the rhs Node's attribute references.
+  def substitute(rhs)
+  end
+
+  # data().
+  # @abstract
+  # Getter. Gets the data.
+  # @return [] the node's data attribute.
+  def data()
+  end
+
+  # back().
+  # @abstract
+  # Getter. Gets the back node.
+  # @return [Node] back
+  # The preceding node.
+  def back()
+  end
+
+  # front().
+  # @abstract
+  # Getter. Gets the front node.
+  # @return [Node] front
+  # The leading node.
+  def front()
+  end
+
+  # type().
+  # @abstract
+  # Getter. Gets the data's type.
+  def type()
+  end
+
+  # ==(rhs).
+  # @abstract
+  # Attribute equality operator.
+  # @param [Node] rhs
+  # A comparison Node instance.
+  # @return [TrueClass, FalseClass]
+  # True in the case the lhs and the rhs's attribute values are equal, and
+  # false otherwise.
+  def ==(rhs)
+  end
+
+  # ===(rhs).
+  # @abstract
+  # Case equality operator.
+  # @param [Node] rhs
+  # A comparing Node instance.
+  # @return [TrueClass, FalseClass]
+  # True in the case the lhs and rhs objects refer the same object.
+  def ===(rhs)
+  end
+
+  # inspect().
+  # @abstract
+  # Returns a node representation string.
+  def inspect()
+  end
+
+  # to_s().
+  # @abstract
+  # Node, String conversion.
+  # @return [String]
+  # self's String representation.
+  def to_s()
+  end
+
+  private
+
+  # back=(back).
+  # @abstract
+  # Setter. Sets the preceding node.
+  def back=(back)
+  end
+
+  # front=(front).
+  # @abstract
+  # Setter. Sets the front node.
+  # @param [Node] front
+  # The object becoming the front node.
+  def front=(front)
+  end
+
+  # data=(data).
+  # @abstract
+  # Setter. Sets the data attribute.
+  # @param [Object] data
+  # The node's data.
+  def data=(data)
+  end
+
+  # initialize_copy().
+  # @abstract
+  # Copy constructor.
+  # @return [Node] copy
+  # A self copy. The object and attribute references differ.
+  def initialize_copy()
+  end
+
 end
--- test/node_int_test.rb
+++ test/node_int_test.rb
@@ -16,6 +16,81 @@ class NodeIntTest < Minitest::Test
   # @abstract
   # Set fixtures.
   def setup()
+    @plain_instance = NodeInt.new()
+  end
+
+  # test_data_getter_declared().
+  # @abstract
+  # The data attribute getter was declared.
+  def test_data_getter_declared()
+    assert_respond_to(@plain_instance, 'data')
+  end
+
+  # test_equality_operator_declared().
+  # @abstract
+  # The equality operator was declared.
+  def test_equality_operator_declared()
+    assert_respond_to(@plain_instance, '==')
+  end
+
+  # test_insepct_declared().
+  # @abstract
+  # The inspect method was declared.
+  def test_inspect_declared()
+    assert_respond_to(@plain_instance, 'inspect')
+  end
+
+  # test_type_getter_declared().
+  # @abstract
+  # The type attribute getter method was declared.
+  def test_type_getter_declared()
+    assert_respond_to(@plain_instance, 'type')
+  end
+
+  # test_back_getter_declared().
+  # @abstract
+  # The back getter method was declared.
+  def test_back_getter_declared()
+    assert_respond_to(@plain_instance, 'back')
+  end
+
+  # test_front_getter_declared().
+  # @abstract
+  # The front attribute getter method was declared.
+  def test_front_getter_declared()
+    assert_respond_to(@plain_instance, 'front')
+  end
+
+  # test_caseeqop_declared().
+  # @abstract
+  # The case equality operator was declared.
+  def test_caseeqop_declared()
+    assert_respond_to(@plain_instance, '===')
+  end
+
+  # test_substitute_declared().
+  # @abstract
+  # The assignment method was declared.
+  def test_substitute_declared()
+    assert_respond_to(@plain_instance, 'substitute')
+  end
+
+  # test_string_con_declared().
+  # @abstract
+  # The string conversion method was declared.
+  def test_string_con_declared()
+    assert_respond_to(@plain_instance, 'to_s')
+  end
+
+  # test_private_methods_declared().
+  # @abstract
+  # The private methods were declared.
+  def test_private_methods_declared()
+    p_methods = @plain_instance.private_methods(all = false)
+    assert_includes(p_methods, :back=)
+    assert_includes(p_methods, :front=)
+    assert_includes(p_methods, :data=)
+    assert_includes(p_methods, :initialize)
   end
 
   # teardown().
